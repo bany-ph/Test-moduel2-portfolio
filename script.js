38f639cf569ab5edb9bb3f01acca09b154c011c9
bany-ph <brisbanystudy@gmail.com>
2025-06-11 12:56:43 -0500

    feat: add color theme change

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const local = localStorage.getItem('theme');
+
+if (local && local == 'light') {
+    const html = document.querySelector("html")
+    html.classList.add('light')
+}else{
+    const html = document.querySelector("html")
+    html.classList.remove('light')
+}
+
+function changeTheme(){
+    const html = document.querySelector("html")
+    const theme = localStorage.getItem("theme")
+
+    if (theme && theme == "light") {
+        localStorage.removeItem("theme")
+        
+        html.classList.remove("light")
+    } else {
+        html.classList.add("light") 
+        localStorage.setItem("theme", "light")
+    }
+}
